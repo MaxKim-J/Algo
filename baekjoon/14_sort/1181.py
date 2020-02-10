36632ce56e20180e841a07190616b7f85b095969
Max Jonghyuk Kim <hwaseen@gmail.com>
2020-02-10 12:42:42 +0900

    백준 1181번 풀이

---
+++ baekjoon/14_sort/1181.py
@@ -0,0 +1,27 @@
+# 백준 1181번 단어정렬
+
+# test_num = 13
+# test_arr = ['but', 'i', 'wont', 'hesitate', 'no', 'more',
+#             'no', 'more', 'it', 'cannot', 'wait', 'im', 'yours']
+
+num = int(input())
+
+arr = []
+for _ in range(num):
+    figure = input()
+    arr.append(figure)
+
+len_dict = {}
+for elem in arr:
+    try:
+        if (len_dict[len(elem)]) and (elem not in len_dict[len(elem)]):
+            len_dict[len(elem)].append(elem)
+    except KeyError:
+        len_dict[len(elem)] = [elem]
+
+for arr in sorted(len_dict.items()):
+    if len(arr[1]) == 1:
+        print(arr[1][0])
+    else:
+        for elem in sorted(arr[1]):
+            print(elem)
