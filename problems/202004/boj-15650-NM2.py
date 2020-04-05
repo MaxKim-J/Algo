df81585d619e0222f57bceccbdefa41b944ef651
김종혁 <hwaseen@gmail.com>
2020-04-06 00:28:34 +0900

    백준 15650번 <N과 M(2)> 풀이 개선 경과 : 맞았습니다(56ms)

--- problems/202004/boj-15650-NM2.py
+++ problems/202004/boj-15650-NM2.py
@@ -8,13 +8,9 @@ def DFS(num_list, result, count):
     if count == 0:
         print(result)
         return
-
     for idx, num in enumerate(num_list):
-        current_result = result
-        current_result += f"{str(num)} "
-        current_num_list = num_list[idx:]
-        current_num_list.pop(0)
-        DFS(current_num_list, current_result, count - 1)
+        current_num_list = num_list[idx+1:]
+        DFS(current_num_list, result + f"{str(num)} ", count - 1)
 
 
 N, M = map(int, input().split())
@@ -23,3 +19,5 @@ DFS(num_list, "", M)
 
 #! 중점1 ) 값을 그대로 가져가는게 아니라 복사하는게 중요하다
 # * 왜? 재귀로 부를때마다 다른 result가 필요하기 때문이라고 생각하면 편할듯?
+
+#! 이건 이터툴보다 빠르네???(요상하다)
