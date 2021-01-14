fc752e88c021ddd37173fa377777a93399fc8ab2
Max Kim <hwaseen@gmail.com>
2021-01-14 23:49:01 +0900

    PS:백준 1987 <알파벳> BFS풀이 수정

--- problems/202101/R-boj-1987-alphabet.py
+++ problems/202101/R-boj-1987-alphabet.py
@@ -2,15 +2,15 @@
 백준 1987 알파벳
 
 - 이 문제는 BFS, DFS, 단순 백트랙킹 등 백트랙킹을 하는 모든 방법으로 풀 수 있다
-- 탐색을 하는데 있어서 항상 뭐 꼭 그래프순회식으로 할 필요는 없다
+- 탐색을 하는데 있어서 항상 뭐 꼭 그래프 순회식으로 할 필요는 없다
 - 이 문제는 BFS로 하는게 성능이 월등히 좋았음 => 최적 depth는 일단 BFS로 가자구
-- 연산 : 참 이게 백트랙킹 했을 때의 경우의수를 생각해내기가 힘듬
+- 연산 : 참 이게 백트랙킹 했을 때의 경우의 수를 생각해내기가 힘듬
     - 최대 문자판의 크기 20*20이고 최악의 경우 모든 칸(400개) 다 방문할 수 있을 때 가장 많은 연산을 할 것
     - 대충 4방향으로 다 갈 수 있다고 생각하면 4 + 4**2 + 4**3 + ... + 4**400 => 완전탐색으로는 힘들고 백트랙킹 필요
     - 백트랙킹을 했을 경우 : 이미 갔던 알파벳은 순회하지 않으므로, 알파벳에 시간복잡도가 의존하게 됨
-    - 백트랙킹 방법 : 알파벳은 총 26개이므로 최대 depth는 26 + 그리고 경계 근처의 칸에서는 4가 아니라 3, 2개의 순회 가능
+    - 백트랙킹 방법 : 알파벳은 총 26개이므로 최대 depth는 26(4^26) + 그리고 경계 근처의 칸에서는 4가 아니라 3, 2개의 순회 가능
     - 이런 꼴일때는 시간복잡도를 정확하게 측정하기 어렵긴 하다 입력의 유형에 따라 천차만별이라서
-    - 여기까지 생각하고 대충 많이 줄겠군 + 어떻게 해야 답에 빨리 도달하겠군 => BFS, DFS, 재귀 백트랙킹 중 편한 방법으로
+    - 여기까지 생각하고 대충 많이 줄겠군 + 어떻게 해야 답에 빨리 도달하겠군 => BFS, DFS, 재귀 백트랙킹 중 편한 방법으로 + DP
 '''
 
 
@@ -23,22 +23,21 @@ dy = (1, 0, -1, 0)
 
 def bfs():
     result = 0
-    # * visit에 바로 append하는 방법과 비슷한 방법인듯
-    # 이런식으로 같은 경우를 한번만 방문 가능(depth가 늘어나면 어짜피 같은 경우는 없으니)
-    queue = set()
-    queue.add((0, 0, board[0][0]))
+    queue = deque([(0, 0, board[0][0])])
+    visited = dict()
     while queue:
-        x, y, sentence = queue.pop()
+        x, y, sentence = queue.popleft()
         result = max(result, len(sentence))
-        # 알파벳을 모두 한번씩 지나면 땡이므로
-        #! 사실 나올 수 있는 최대 암호 자리는 26, 트리의 최대 뎁쓰도 26 => 알파벳 개수가
         if result == 26:
             return 26
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
-            # 바로 그냥 암호에 있는지 없는지 판단함. visit을 따로 둘 필요가 없음
             if (-1 < nx < R) and (-1 < ny < C) and (board[nx][ny] not in sentence):
-                queue.add((nx, ny, sentence + board[nx][ny]))
+                # x,y,value가 같은 노드는 단 한번만 돌면 된다(위 value까지 다 같다는 뜻이므로)
+                current = (nx, ny, sentence + board[nx][ny])
+                if not visited.get(current):
+                    queue.append(current)
+                    visited[current] = 1
     return result
 
 
@@ -94,6 +93,9 @@ print(ans)
 백트랙킹 풀이 => 걍 단순하게는 이렇게도 된다
 굉장히 쉽고 직관적인 풀이가 되었다
 
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
 def backt(x, y, word):
     global result
     check = 0
@@ -111,8 +113,6 @@ def backt(x, y, word):
         return
 
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 n, m = map(int, input().split())  # R, C | 세로, 가로
 words = [list(input()) for _ in range(n)]
 result = 0
