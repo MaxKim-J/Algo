28f4f694fecabf9f52dabc17b60d5b41b45e33a9
Max Kim <hwaseen@gmail.com>
2021-02-13 00:52:50 +0900

    PS:백준 9465 <스티커> 풀이

---
+++ problems/202102/R-boj-9465-스티커.py
@@ -0,0 +1,23 @@
+# 왼쪽에서 오른쪽으로
+# 순서가 별로 상관없는 입력이라도, 순서와 상관있게 만들고 생각하기
+# 받은 입력 그대로 DP
+#! 테이블의 x => 얘가 마지막으로 선택된 요소일때 앞에서 뭐를 뽑았을지를 생각하기(이런 유형 많은듯..)
+'''
+귀납적 DP...
+https://sungmin-joo.tistory.com/42
+경우의 수를 일단 생각해봐야함 그게 먼저임 => 어디서 시작해서 어디로 갈 수 있는 경우의 수 그게 dp(n)
+그리고 그거 몇개 구해놓은 다음에 dp(n-1)이랑 dp(n-2)를 살펴볼것!
+'''
+
+t = int(input())
+for i in range(t):
+    dp = []
+    n = int(input())
+    for _ in range(2):
+        dp.append(list(map(int, input().split())))
+    dp[0][1] += dp[1][0]
+    dp[1][1] += dp[0][0]
+    for j in range(2, n):
+        dp[0][j] += max(dp[1][j - 1], dp[1][j - 2])
+        dp[1][j] += max(dp[0][j - 1], dp[0][j - 2])
+    print(max(dp[0][n - 1], dp[1][n - 1]))
