55cc9b780a85158b0bc23276bbcade2643003f78
Max Kim <hwaseen@gmail.com>
2021-02-12 23:56:06 +0900

    PS:백준 2193번 <이친수> 숏코딩으로 풀이 수정

--- problems/202102/boj-2193-이친수.py
+++ problems/202102/boj-2193-이친수.py
@@ -1,14 +1,13 @@
 # 이천수..?
 
 N = int(input())
-dp = [1, 0]
 
 if N < 3:
     print(1)
     exit(0)
 
+dp = [1, 0]
 for _ in range(2, N):
-    temp = [dp[0] + dp[1], dp[0]]
-    dp = temp
+    dp = [dp[0] + dp[1], dp[0]]
 
 print(sum(dp))
