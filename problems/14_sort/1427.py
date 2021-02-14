bdab4d6aa69e65fb26998d608e9b07ea9091d6c8
Max Kim <hwaseen@gmail.com>
2021-02-14 23:49:10 +0900

    README:리드미 수정 및 폴더 구조 정리

--- problems/14_sort/1427.py
+++
@@ -1,9 +0,0 @@
-arr = input()
-num_arr = []
-for num in arr:
-    num_arr.append(int(num))
-num_arr.sort(reverse=True)
-result = ''
-for num in num_arr:
-    result += str(num)
-print(result)
--- problems/else/1712.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-A, B, C = map(int, sys.stdin.readline().split())
-
-if C <= B:
-    print(-1)
-else:
-    result = A /(C - B)
-    print(int(result)+1)
