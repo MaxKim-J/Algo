782e892a5fe233fdd456b9edcfd1784eec1dc976
Max Kim <hwaseen@gmail.com>
2021-02-21 22:08:05 +0900

    PS:백준 11728번 <배열 합치기> 풀이 수정

--- problems/202102/boj-11728-배열합치기.py
+++ problems/202102/boj-11728-배열합치기.py
@@ -1,8 +1,19 @@
 '''
-오히려 이게 시간초과가 나고
-sorted 사용하는게 시간초과가 안난다(...?)
+병합정렬할때 그 포인터로 하는 정렬방법
+배열을 직접 modify하는건 시간초과나고
+가장 빠른 방법은 
+
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+for i in sorted(A+B):
+    print(i, end=' ')
+
+이거였음ㅋ
 '''
 
+# 그나마 돌아가는 방법이면서 문제의 의도를 충족하는 해답은 이거임
 
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
@@ -10,21 +21,24 @@ B = list(map(int, input().split()))
 
 result = []
 
+a = 0
+b = 0
+
 for _ in range(2000000):
-    if len(A) == 0:
-        for j in B:
-            result.append(j)
+    if a > len(A) - 1:
+        result.extend(B[b:])
         break
 
-    if len(B) == 0:
-        for j in A:
-            result.append(j)
+    if b > len(B) - 1:
+        result.extend(A[a:])
         break
 
-    if A[0] > B[0]:
-        result.append(B.pop(0))
-    elif A[0] <= B[0]:
-        result.append(A.pop(0))
+    if A[a] > B[b]:
+        result.append(B[b])
+        b += 1
+    elif A[a] <= B[b]:
+        result.append(A[a])
+        a += 1
 
 for i in result:
     print(i, end=' ')
