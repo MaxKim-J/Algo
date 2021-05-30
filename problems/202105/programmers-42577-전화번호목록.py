761cfeb21cafd92b4513562395b3c1c27fd9b6b8
Max Kim <hwaseen@gmail.com>
2021-05-31 00:50:49 +0900

    PS:프로그래머스 42577번 <전화번호 목록> 오류 수정

--- problems/202105/programmers-42577-전화번호목록.py
+++ problems/202105/programmers-42577-전화번호목록.py
@@ -10,8 +10,8 @@ def solution(phone_book):
         temp = ""
         # 번호 배열을 순회하고 순회하는 번호를 하나씩 뽑아서 temp를 만들고
         # temp가 hash_map에 있는지 확인한다.
-        for number in phone_number:
-            temp += number
-            if temp in hash_map and temp != phone_number:
-                answer = False
+    for num in phone_number:
+        temp += num
+        if temp in hash_map and temp != phone_number:
+          answer = False
     return answer
