a62bd7235951aa0e6777fc76c9da51cf50c62bee
Max Kim <hwaseen@gmail.com>
2021-09-14 00:06:48 +0900

    PS:카카오 블라인드 2022 <양궁> 풀이

---
+++ problems/202109/kakao-2021blind-양궁.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+n = int(input())
+info=list(map(int, input().split()))
+
+answer=[]
+
+def get_score(string):
+  apeach = 0
+  lion = 0
+  arr = list(map(int, list(string)))
+  for i in range(10):
+    if info[i] >= arr[i]:
+      if (arr[i] == 0) and (info[i] == 0):
+        continue
+      apeach += (10 - i)
+    elif arr[i] > info[i]:
+      lion += (10 - i)
+
+  return (lion > apeach, lion-apeach)
+
+def dfs():
+  queue = deque([(n, '')])
+
+  while queue:
+    left, string = queue.pop()
+
+    if len(string) == 11:
+      continue
+
+    if left == 0:
+      new_string = string + '0' * (10 - len(string))
+      is_win, score = get_score(new_string)
+      if is_win:
+        answer.append((score, new_string))
+
+    for j in range(left+1):
+      new_string = string + str(j)
+      queue.append((left-j, new_string))
+
+dfs()
+
+answer = sorted(answer, key=lambda x:(-x[0], x[1]))
+
+if len(answer) == 0:
+  print([-1])
+else:
+  print(list(map(int, answer[0][1])))
