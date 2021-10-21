365a0a2dbe1c6b7a1bd71fb6eb05ab20a9dd71e6
Max Kim <hwaseen@gmail.com>
2021-10-21 17:00:17 +0900

    PS:손코딩 연습 <주식 가격> 풀이 첨언

--- problems/202110/handCoding-주식가격.js
+++ problems/202110/handCoding-주식가격.js
@@ -21,23 +21,15 @@ const getMaxIndex = (arr, s) => {
   return resultIndex;
 };
 
+// 최선의 경우 O(n), 최악의 경우 O(n^2)
 const getMaxProfit = (stockPrices) => {
   let start = 0;
-  const leftPrices = [];
-  let result = -Infinity;
+  let result = 0;
 
   while (start < stockPrices.length - 1) {
     const maxIndex = getMaxIndex(stockPrices, start);
-    if (maxIndex === start) {
-      const secondMaxIndex = getMaxIndex(stockPrices, start + 1);
-      result = Math.max(
-        result,
-        stockPrices[secondMaxIndex] - stockPrices[maxIndex]
-      );
-    } else {
-      for (let i = start; i < maxIndex; i++) {
-        result = Math.max(result, stockPrices[maxIndex] - stockPrices[i]);
-      }
+    for (let i = start; i < maxIndex; i++) {
+      result = Math.max(result, stockPrices[maxIndex] - stockPrices[i]);
     }
     start = maxIndex + 1;
   }
@@ -45,8 +37,26 @@ const getMaxProfit = (stockPrices) => {
   return result;
 };
 
+// 모든 경우에 O(n)
+const getMaxProfitFullyOptimized = (stockPrices) => {
+  let minPrice = stockPrices[0];
+  let maxProfit = 0;
+
+  for (let i = 1; i < stockPrices.length; i++) {
+    const profit = stockPrices[i] - minPrice;
+    if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+    if (minPrice > stockPrices[i]) {
+      minPrice = stockPrices[i];
+    }
+  }
+
+  return maxProfit;
+};
+
 const ex1 = [8000, 7000, 8000, 10000, 5000, 7000]; // 중간쯤
 const ex2 = [1000, 2000, 3000, 4000, 5000, 10000]; // O(n)
 const ex3 = [10000, 9000, 8000, 7000, 6000, 5000]; // O(n^2)
 
-console.log(getMaxProfit(ex2));
+console.log(getMaxProfitFullyOptimized(ex1));
